b1cf0ad5af7724fd7e8a482a3e4a1648d29b7146
David Rubin <daviru007@icloud.com>
2024-09-17 00:07:43 -0700

    improve the benchmarks

--- plot.py
+++ plot.py
@@ -2,11 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr  
 import pandas as pd
 
+def sizeof_fmt(x, pos):
+    if x<0:
+        return ""
+    for x_unit in ['bytes', 'kB', 'MB', 'GB', 'TB']:
+        if x < 1024:
+            return "%3.0f %s" % (x, x_unit)
+        x /= 1024
+
+# Main function
 def main():
+    # Read the file
     file = open("data.txt", "r")
     lines = file.readlines()
 
-    data = []
+    start_data = []
+    middle_data = []
+    end_data = []    
 
     for i in range(0, len(lines), 1):
         line = lines[i]
@@ -14,42 +26,59 @@ def main():
 
         size = int(line[0])
 
-        new_time = float(line[1])
-        old_time = float(line[2])
+        start_new = int(line[1])
+        start_old = int(line[2])
+        start_data.append([size, start_new, start_old])
 
-        data.append([size, new_time, old_time])
+        middle_new = int(line[3])
+        middle_old = int(line[4])
+        middle_data.append([size, middle_new, middle_old])
 
+        end_new = int(line[5])
+        end_old = int(line[6])
+        end_data.append([size, end_new, end_old])
     file.close()
 
-    df = pd.DataFrame(data, columns=["size", "new", "old"])
+    start_df = pd.DataFrame(start_data, columns=["size", "new", "old"])
+    middle_df = pd.DataFrame(middle_data, columns=["size", "new", "old"])
+    end_df = pd.DataFrame(end_data, columns=["size", "new", "old"])
 
-    df_to_plot = df
+    # Smooth the data by taking a rolling average of 10 points for the "new" and "old" columns
+    start_df["new"] = start_df["new"].rolling(window=10).mean()
+    start_df["old"] = start_df["old"].rolling(window=10).mean()
 
-    plt.plot(df_to_plot["size"], df_to_plot["new"].rolling(20).median(), label="new")
-    plt.plot(df_to_plot["size"], df_to_plot["old"].rolling(20).median(), label="old")
-    
+    middle_df["new"] = middle_df["new"].rolling(window=10).mean()
+    middle_df["old"] = middle_df["old"].rolling(window=10).mean()
 
-    plt.xlabel("size(bytes)")
-    plt.ylabel("runtime(ns)")
-    
-    plt.xscale('log')
-    plt.yscale('log')
+    end_df["new"] = end_df["new"].rolling(window=10).mean()
+    end_df["old"] = end_df["old"].rolling(window=10).mean()
 
-    simple_formatter = lambda x, _: "%0.1f" % x
-    
-    
-    plt.gca().yaxis.set_major_formatter(tkr.FuncFormatter(simple_formatter))
-    plt.gca().yaxis.set_minor_formatter(tkr.FuncFormatter(simple_formatter))
-    plt.gca().grid(True, which="both", axis="y", linestyle="--", linewidth=1)
-    plt.gca().grid(True, which="major", axis="x", linestyle="--", linewidth=1)
-    
-    plt.gca().yaxis.set_major_locator(tkr.LogLocator(base=10.0, subs=[], numticks=1))
-    plt.gca().yaxis.set_minor_locator(tkr.LogLocator(base=10.0, subs=range(10), numticks=1))
+    # Plot each DataFrame as a seperate plot using subplots. So, 3x1 grid of plots
+    fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+
+    # Plot the start data
+    ax[0].plot(start_df["size"], start_df["new"], label="new")
+    ax[0].plot(start_df["size"], start_df["old"], label="old")
+    ax[0].set_title("Point at the start")
+    ax[0].xlabel = "Items"
+
+    # Plot the middle data
+    ax[1].plot(middle_df["size"], middle_df["new"], label="new")
+    ax[1].plot(middle_df["size"], middle_df["old"], label="old")
+    ax[1].set_title("Point at the middle")
+
+    # Plot the end data
+    ax[2].plot(end_df["size"], end_df["new"], label="new")
+    ax[2].plot(end_df["size"], end_df["old"], label="old")
+    ax[2].set_title("Point at the end")
+
+
+    plt.xlabel("Items")
+    plt.ylabel("Cycles")
+
     plt.legend()
-
-    plt.title("partition point benchmark")
-
     plt.savefig("graph.png")
-    plt.show()
 
-if __name__ == "__main__": main()
+
+
+main()
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn partitionPoint(
+noinline fn partitionPoint(
     comptime T: type,
     items: []const T,
     context: anytype,
@@ -20,7 +20,7 @@ pub fn partitionPoint(
     return low;
 }
 
-pub fn partitionPointNew(
+noinline fn partitionPointNew(
     comptime T: type,
     items: []const T,
     context: anytype,
@@ -45,208 +45,87 @@ pub fn partitionPointNew(
     return it;
 }
 
-fn testPartitionPointImpl(partitionPointImpl: anytype) !void {
-    const S = struct {
-        fn lowerU32(context: u32, item: u32) bool {
-            return item < context;
-        }
-        fn lowerI32(context: i32, item: i32) bool {
-            return item < context;
-        }
-        fn lowerF32(context: f32, item: f32) bool {
-            return item < context;
-        }
-        fn lowerEqU32(context: u32, item: u32) bool {
-            return item <= context;
-        }
-        fn lowerEqI32(context: i32, item: i32) bool {
-            return item <= context;
-        }
-        fn lowerEqF32(context: f32, item: f32) bool {
-            return item <= context;
-        }
-        fn isEven(_: void, item: u8) bool {
-            return item % 2 == 0;
-        }
-    };
-
-    try std.testing.expectEqual(0, partitionPointImpl(u32, &[_]u32{}, @as(u32, 0), S.lowerU32));
-    try std.testing.expectEqual(0, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 0), S.lowerU32));
-    try std.testing.expectEqual(0, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 2), S.lowerU32));
-    try std.testing.expectEqual(2, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 5), S.lowerU32));
-    try std.testing.expectEqual(2, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 8), S.lowerU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 7, 7, 7, 7, 16, 32, 64 }, @as(u32, 8), S.lowerU32));
-    try std.testing.expectEqual(2, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 8, 8, 8, 16, 32, 64 }, @as(u32, 8), S.lowerU32));
-    try std.testing.expectEqual(5, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 64), S.lowerU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 100), S.lowerU32));
-    try std.testing.expectEqual(2, partitionPointImpl(i32, &[_]i32{ 2, 4, 8, 16, 32, 64 }, @as(i32, 5), S.lowerI32));
-    try std.testing.expectEqual(1, partitionPointImpl(f32, &[_]f32{ -54.2, -26.7, 0.0, 56.55, 100.1, 322.0 }, @as(f32, -33.4), S.lowerF32));
-    try std.testing.expectEqual(0, partitionPointImpl(u32, &[_]u32{}, @as(u32, 0), S.lowerEqU32));
-    try std.testing.expectEqual(0, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 0), S.lowerEqU32));
-    try std.testing.expectEqual(1, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 2), S.lowerEqU32));
-    try std.testing.expectEqual(2, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 5), S.lowerEqU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 7, 7, 7, 7, 16, 32, 64 }, @as(u32, 8), S.lowerEqU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 8, 8, 8, 16, 32, 64 }, @as(u32, 8), S.lowerEqU32));
-    try std.testing.expectEqual(3, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 8), S.lowerEqU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 64), S.lowerEqU32));
-    try std.testing.expectEqual(6, partitionPointImpl(u32, &[_]u32{ 2, 4, 8, 16, 32, 64 }, @as(u32, 100), S.lowerEqU32));
-    try std.testing.expectEqual(2, partitionPointImpl(i32, &[_]i32{ 2, 4, 8, 16, 32, 64 }, @as(i32, 5), S.lowerEqI32));
-    try std.testing.expectEqual(1, partitionPointImpl(f32, &[_]f32{ -54.2, -26.7, 0.0, 56.55, 100.1, 322.0 }, @as(f32, -33.4), S.lowerEqF32));
-    try std.testing.expectEqual(4, partitionPointImpl(u8, &[_]u8{ 0, 50, 14, 2, 5, 71 }, {}, S.isEven));
-}
-
-test partitionPoint {
-    try testPartitionPointImpl(partitionPoint);
-}
-
-test partitionPointNew {
-    try testPartitionPointImpl(partitionPointNew);
-}
-
 const allocator = std.heap.c_allocator;
 
 const iterations_per_byte = 1024;
 const warmup_iterations = 64;
-const repeats = 16;
 
-comptime {
-    std.debug.assert(iterations_per_byte % repeats == 0);
-    std.debug.assert(warmup_iterations % repeats == 0);
-}
-
-// #20357
-pub fn sched_setaffinity(pid: std.os.linux.pid_t, set: *const std.os.linux.cpu_set_t) !void {
-    const size = @sizeOf(std.os.linux.cpu_set_t);
-    const rc = std.os.linux.syscall3(.sched_setaffinity, @as(usize, @bitCast(@as(isize, pid))), size, @intFromPtr(set));
-
-    switch (std.posix.errno(rc)) {
-        .SUCCESS => return,
-        else => |err| return std.posix.unexpectedErrno(err),
-    }
-}
 const Tp = u32;
 
-fn lower(context: Tp, item: Tp) bool {
-    return item < context;
+fn lower(other: Tp, item: Tp) bool {
+    return item <= other;
 }
 
 pub fn main() !void {
     // Pin the process to a single core (1)
     if (@import("builtin").os.tag == .linux) {
         const cpu0001: std.os.linux.cpu_set_t = [1]usize{0b0001} ++ ([_]usize{0} ** (16 - 1));
-        try sched_setaffinity(0, &cpu0001);
+        try std.os.linux.sched_setaffinity(0, &cpu0001);
     }
 
-    const max_bytes = 1 << 24;
+    const items: u32 = 10e3;
     var file = try std.fs.cwd().createFile("data.txt", .{});
     defer file.close();
-    const stdout = file;
+    const stdout = file.writer();
 
-    // var rng = std.Random.DefaultPrng.init(0);
-    // const rand = rng.random();
+    const total_buffer = try allocator.alloc(Tp, items);
+    defer allocator.free(total_buffer);
 
-    var N: usize = 1;
-    var buffer_array_list = std.ArrayList(Tp).init(allocator);
-    defer buffer_array_list.deinit();
+    for (0..items) |N| {
+        try stdout.print("{},", .{N});
 
-    const queries = try allocator.alloc(Tp, iterations_per_byte + warmup_iterations);
-    defer allocator.free(queries);
+        const buffer = total_buffer[0..N];
 
-    var new_sum_logs: f64 = 0;
-    var old_sum_logs: f64 = 0;
-    var num_entries: f64 = 0;
+        inline for (.{ 1, N / 2, N - 1 }) |point| {
+            const other: u32 = @intCast(point);
 
-    const Factor = 200;
+            for (0..N) |i| buffer[i] = @intCast(i);
+            clflush(Tp, buffer);
 
-    while (N < max_bytes) {
-        defer N = (N * (Factor + 1) + Factor - 1) / Factor;
-
-        buffer_array_list.clearRetainingCapacity();
-        const buffer = try buffer_array_list.addManyAsSlice(N);
-
-        for (0..N) |i| buffer[i] = @intCast(i);
-        clflush(Tp, buffer);
-
-        // uniform over whole range
-        // for (queries) |*e| e.* = rand.intRangeAtMost(Tp, 0, @intCast(N));
-
-        // uniform over first 16
-        // for (queries) |*e| e.* = rand.intRangeAtMost(Tp, 0, @intCast(@min(N, 16)));
-
-        // first
-        // for (queries) |*e| e.* = 0;
-
-        // one past the end
-        for (queries) |*e| e.* = @intCast(N);
-
-
-        // mix of first, one past the end, and uniform
-        // for (queries) |*e| e.* = switch (rand.int(u2)) {
-        //     0 => 0,
-        //     1, 2 => rand.intRangeAtMost(Tp, 0, @intCast(N)),
-        //     3 => @intCast(N),
-        // };
-
-        var new_i: u32 = 0;
-        var new_ns: usize = 0;
-        while (new_i < iterations_per_byte + warmup_iterations) : (new_i += repeats) {
-            const start = std.time.Instant.now() catch unreachable;
-
-            for (0..repeats) |offs| {
+            var new_i: u32 = 0;
+            var new_ns: usize = 0;
+            while (new_i < iterations_per_byte + warmup_iterations) : (new_i += 1) {
+                const start = rdtsc();
                 std.mem.doNotOptimizeAway(partitionPointNew(
                     Tp,
                     buffer,
-                    queries[new_i + offs],
+                    other,
                     lower,
                 ));
+                const end = rdtsc();
+                if (new_i > warmup_iterations) new_ns += end - start;
             }
 
-            const end = std.time.Instant.now() catch unreachable;
-            if (new_i > warmup_iterations) new_ns += end.since(start);
-        }
+            for (0..N) |i| buffer[i] = @intCast(i);
+            clflush(Tp, buffer);
 
-        for (0..N) |i| buffer[i] = @intCast(i);
-        clflush(Tp, buffer);
-
-        var old_i: u32 = 0;
-        var old_ns: usize = 0;
-        while (old_i < iterations_per_byte + warmup_iterations) : (old_i += repeats) {
-            const start = std.time.Instant.now() catch unreachable;
-
-            for (0..repeats) |offs| {
+            var old_i: u32 = 0;
+            var old_ns: usize = 0;
+            while (old_i < iterations_per_byte + warmup_iterations) : (old_i += 1) {
+                const start = rdtsc();
                 std.mem.doNotOptimizeAway(partitionPoint(
                     Tp,
                     buffer,
-                    queries[old_i + offs],
+                    other,
                     lower,
                 ));
+                const end = rdtsc();
+                if (old_i > warmup_iterations) old_ns += end - start;
             }
 
-            const end = std.time.Instant.now() catch unreachable;
-            if (old_i > warmup_iterations) old_ns += end.since(start);
+            const new_cycles_per_byte = new_ns / iterations_per_byte;
+            const old_cycles_per_byte = old_ns / iterations_per_byte;
+
+            try stdout.print("{d},{d},", .{
+                new_cycles_per_byte,
+                old_cycles_per_byte,
+            });
         }
 
-        const new_cycles_per_byte = @as(f64, @floatFromInt(new_ns)) / @as(f64, @floatFromInt(iterations_per_byte));
-        const old_cycles_per_byte = @as(f64, @floatFromInt(old_ns)) / @as(f64, @floatFromInt(iterations_per_byte));
+        try stdout.writeAll("\n");
 
-        new_sum_logs += @log(new_cycles_per_byte);
-        old_sum_logs += @log(old_cycles_per_byte);
-        num_entries += 1.0;
-
-        try stdout.writer().print("{},{d:.4},{d:.4}\n", .{
-            N * @sizeOf(Tp),
-            new_cycles_per_byte,
-            old_cycles_per_byte,
-        });
+        if (N % 1000 == 0) std.debug.print("N: {d}/{d}\n", .{ N, items });
     }
-
-    const new_geo_mean = @exp(new_sum_logs / num_entries);
-    const old_geo_mean = @exp(old_sum_logs / num_entries);
-
-    const time_reduction = (old_geo_mean - new_geo_mean) / old_geo_mean * 100;
-    const speedup_percent = old_geo_mean / new_geo_mean * 100 - 100;
-
-    std.debug.print("speedup: {d:.0}% (time reduced by average of {d:.0}%) (NOTE: *almost* meaningless metric, see graph)\n", .{ speedup_percent, time_reduction });
 }
 
 inline fn rdtsc() usize {
